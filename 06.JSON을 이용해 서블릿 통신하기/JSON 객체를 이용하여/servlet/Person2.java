a8e560805fb72459badc2c314e0c4024c5b1b913
msnodeve <msnodeve@gmail.com>
2019-05-24 12:05:46 +0900

    Map 객체 이용하여 JSON 통신

--- 06.JSON을 이용해 서블릿 통신하기/JSON 객체를 이용하여/servlet/Person2.java
+++
@@ -1,8 +0,0 @@
-public class Person2 {
-	public String hakbun;
-	public String name;
-	public int age;
-	public String tel;
-	public String jumin;
-	public String address;
-}
--- test/MyEventListener.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public interface MyEventListener {
-	//public void complete(ArrayList<Person> list);
-	public void complete(Object object);
-	public void complete(String message);
-}
