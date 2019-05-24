776c1eeeae8e421be0a3c1a55867f404c3b3a645
msnodeve <msnodeve@gmail.com>
2019-05-24 13:06:46 +0900

    Servlet에 필요한 Person 객체

---
+++ 06.JSON을 이용해 서블릿 통신하기/JSON 객체를 이용하여/servlet/Person.java
@@ -0,0 +1,9 @@
+//Servlet에 필요한 Person 클래스
+public class Person {
+	public String hakbun;
+	public String name;
+	public int age;
+	public String tel;
+	public String jumin;
+	public String address;
+}
